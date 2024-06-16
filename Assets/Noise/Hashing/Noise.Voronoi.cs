c7b5a4b7165d71a8423ebee699abd6ce754cd218
Daniel Pascual López <uo269728@uniovi.es>
2024-06-16 21:50:46 +0200

    2D Voronoi

--- Assets/Noise/Hashing/Noise.Voronoi.cs
+++ Assets/Noise/Hashing/Noise.Voronoi.cs
@@ -9,6 +9,8 @@ public static partial class Noise {
 		return select(minima, distances, distances < minima);
 	}
 
+    static float4 GetDistance (float4 x, float4 y) => sqrt(x * x + y * y);
+
 	public struct Voronoi1D<L> : INoise where L : struct, ILattice {
 
 		public float4 GetNoise4 (float4x3 positions, SmallXXHash4 hash, int frequency) {
@@ -32,7 +34,22 @@ public static partial class Noise {
 				x = l.GetLatticeSpan4(positions.c0, frequency),
 				z = l.GetLatticeSpan4(positions.c2, frequency);
 
-			return 0f;
+			float4 minima = 2f;
+			for (int u = -1; u <= 1; u++) {
+				SmallXXHash4 hx = hash.Eat(l.ValidateSingleStep(x.p0 + u, frequency));
+				float4 xOffset = u - x.g0;
+				for (int v = -1; v <= 1; v++) {
+					SmallXXHash4 h = hx.Eat(l.ValidateSingleStep(z.p0 + v, frequency));
+					float4 zOffset = v - z.g0;
+					minima = UpdateVoronoiMinima(minima, GetDistance(
+						h.Floats01A + xOffset, h.Floats01B + zOffset
+					));
+					minima = UpdateVoronoiMinima(minima, GetDistance(
+						h.Floats01C + xOffset, h.Floats01D + zOffset
+					));
+				}
+			}
+			return min(minima, 1f);
 		}
 	}
 
