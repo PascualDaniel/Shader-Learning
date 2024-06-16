e119acee94e92b65735a78dc2297ca033fd9140b
Daniel Pascual López <uo269728@uniovi.es>
2024-06-16 22:56:01 +0200

    Tipos de Voronoi

--- Assets/Noise/Hashing/Noise.Voronoi.cs
+++ Assets/Noise/Hashing/Noise.Voronoi.cs
@@ -5,28 +5,39 @@ using static Unity.Mathematics.math;
 public static partial class Noise {
 
 
-    static float4 UpdateVoronoiMinima (float4 minima, float4 distances) {
-		return select(minima, distances, distances < minima);
+    static float4x2 UpdateVoronoiMinima (float4x2 minima, float4 distances) {
+		bool4 newMinimum = distances < minima.c0;
+        minima.c1 = select(
+			select(minima.c1, distances, distances < minima.c1),
+			minima.c0,
+			newMinimum
+		);
+		minima.c0 = select(minima.c0, distances, newMinimum);
+		return minima;
 	}
 
     static float4 GetDistance (float4 x, float4 y) => sqrt(x * x + y * y);
 
-	public struct Voronoi1D<L> : INoise where L : struct, ILattice {
+    static float4 GetDistance (float4 x, float4 y, float4 z) =>
+		sqrt(x * x + y * y + z * z);
+
+        
+	public struct Voronoi1D<L, F> : INoise where L : struct, ILattice where F : struct, IVoronoiFunction {
 
 		public float4 GetNoise4 (float4x3 positions, SmallXXHash4 hash, int frequency) {
 			var l = default(L);
 			LatticeSpan4 x = l.GetLatticeSpan4(positions.c0, frequency);
 
-			float4 minima = 2f;
+			float4x2 minima = 2f;
 			for (int u = -1; u <= 1; u++) {
 				SmallXXHash4 h = hash.Eat(l.ValidateSingleStep(x.p0 + u, frequency));
 				minima = UpdateVoronoiMinima(minima, abs(h.Floats01A + u - x.g0));
 			}
-			return minima;
+			return default(F).Evaluate(minima);
 		}
 	}
 
-	public struct Voronoi2D<L> : INoise where L : struct, ILattice {
+	public struct Voronoi2D<L, F> : INoise where L : struct, ILattice where F : struct, IVoronoiFunction{
 
 		public float4 GetNoise4 (float4x3 positions, SmallXXHash4 hash, int frequency) {
 			var l = default(L);
@@ -34,7 +45,7 @@ public static partial class Noise {
 				x = l.GetLatticeSpan4(positions.c0, frequency),
 				z = l.GetLatticeSpan4(positions.c2, frequency);
 
-			float4 minima = 2f;
+			float4x2 minima = 2f;
 			for (int u = -1; u <= 1; u++) {
 				SmallXXHash4 hx = hash.Eat(l.ValidateSingleStep(x.p0 + u, frequency));
 				float4 xOffset = u - x.g0;
@@ -49,11 +60,13 @@ public static partial class Noise {
 					));
 				}
 			}
-			return min(minima, 1f);
+			minima.c0 = min(minima.c0, 1f);
+			minima.c1 = min(minima.c1, 1f);
+			return default(F).Evaluate(minima);
 		}
 	}
 
-	public struct Voronoi3D<L> : INoise where L : struct, ILattice {
+	public struct Voronoi3D<L, F> : INoise where L : struct, ILattice where F : struct, IVoronoiFunction {
 
 		public float4 GetNoise4 (float4x3 positions, SmallXXHash4 hash, int frequency) {
 			var l = default(L);
@@ -62,7 +75,33 @@ public static partial class Noise {
 				y = l.GetLatticeSpan4(positions.c1, frequency),
 				z = l.GetLatticeSpan4(positions.c2, frequency);
 
-			return 0f;
+			float4x2 minima = 2f;
+			for (int u = -1; u <= 1; u++) {
+				SmallXXHash4 hx = hash.Eat(l.ValidateSingleStep(x.p0 + u, frequency));
+				float4 xOffset = u - x.g0;
+				for (int v = -1; v <= 1; v++) {
+					SmallXXHash4 hy = hx.Eat(l.ValidateSingleStep(y.p0 + v, frequency));
+					float4 yOffset = v - y.g0;
+					for (int w = -1; w <= 1; w++) {
+						SmallXXHash4 h =
+							hy.Eat(l.ValidateSingleStep(z.p0 + w, frequency));
+						float4 zOffset = w - z.g0;
+						minima = UpdateVoronoiMinima(minima, GetDistance(
+							h.GetBitsAsFloats01(5, 0) + xOffset,
+							h.GetBitsAsFloats01(5, 5) + yOffset,
+							h.GetBitsAsFloats01(5, 10) + zOffset
+						));
+						minima = UpdateVoronoiMinima(minima, GetDistance(
+							h.GetBitsAsFloats01(5, 15) + xOffset,
+							h.GetBitsAsFloats01(5, 20) + yOffset,
+							h.GetBitsAsFloats01(5, 25) + zOffset
+						));
+					}
+				}
+			}
+			minima.c0 = min(minima.c0, 1f);
+			minima.c1 = min(minima.c1, 1f);
+			return default(F).Evaluate(minima);
 		}
 	}
 }
--- Assets/Noise/Hashing/SmallXXHash.cs
+++ Assets/Noise/Hashing/SmallXXHash.cs
@@ -68,6 +68,11 @@ public readonly struct SmallXXHash4 {
 	public SmallXXHash4 (uint4 accumulator) {
 		this.accumulator = accumulator;
 	}
+	
+	public uint4 GetBits (int count, int shift) =>
+		((uint4)this >> shift) & (uint)((1 << count) - 1);
+	public float4 GetBitsAsFloats01 (int count, int shift) =>
+		(float4)GetBits(count, shift) * (1f / ((1 << count) - 1));
 
 	public static implicit operator SmallXXHash4 (uint4 accumulator) =>
 		new SmallXXHash4(accumulator);
