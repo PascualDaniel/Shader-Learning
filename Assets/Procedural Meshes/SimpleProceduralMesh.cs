ad98eba75cfba77cb6973a253bbd360172970bd5
Daniel Pascual López <uo269728@uniovi.es>
2024-06-20 23:42:58 +0200

    Procedural meshes

---
+++ Assets/Procedural Meshes/SimpleProceduralMesh.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
+public class SimpleProceduralMesh : MonoBehaviour {
+
+    void OnEnable () {
+		var mesh = new Mesh {
+			name = "Procedural Mesh"
+		};
+        GetComponent<MeshFilter>().mesh = mesh;
+	}
+    
+ }
