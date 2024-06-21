f107a9e615d24261d80330fbcd3a2cf8f9b2e2b5
Daniel Pascual López <uo269728@uniovi.es>
2024-06-21 18:14:10 +0200

    Triangulos

--- Assets/Procedural Meshes/SimpleProceduralMesh.cs
+++ Assets/Procedural Meshes/SimpleProceduralMesh.cs
@@ -7,6 +7,15 @@ public class SimpleProceduralMesh : MonoBehaviour {
 		var mesh = new Mesh {
 			name = "Procedural Mesh"
 		};
+		mesh.vertices = new Vector3[] {
+			Vector3.zero, Vector3.right, Vector3.up
+		};
+		mesh.triangles = new int[] {
+			0, 2, 1
+		};
+		mesh.normals = new Vector3[] {
+			Vector3.back, Vector3.back, Vector3.back
+		};
         GetComponent<MeshFilter>().mesh = mesh;
 	}
     
