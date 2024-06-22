0b948da6b942e8b26caffa80b214adc7bcf878f0
Daniel Pascual López <uo269728@uniovi.es>
2024-06-22 20:40:28 +0200

    Square grid

---
+++ Assets/Procedural Meshes/Streams/SingleStream.cs
@@ -0,0 +1,11 @@
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using Unity.Collections;
+using Unity.Mathematics;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+namespace ProceduralMeshes.Streams {
+
+    public struct SingleStream : IMeshStreams {}
+}
