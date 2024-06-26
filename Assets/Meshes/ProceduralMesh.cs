a4e40dd9bd7a78110f22fa34e4abaf8e2c646dc8
Daniel Pascual López <uo269728@uniovi.es>
2024-06-26 20:28:06 +0200

    Selector de gizmos

--- Assets/Meshes/ProceduralMesh.cs
+++ Assets/Meshes/ProceduralMesh.cs
@@ -7,6 +7,12 @@ using UnityEngine.Rendering;
 [RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
 public class ProceduralMesh : MonoBehaviour {
 
+	[System.Flags]
+	public enum GizmoMode { Nothing = 0, Vertices = 1, Normals = 0b10, Tangents = 0b100 }
+
+	[SerializeField]
+	GizmoMode gizmos;
+
 	Vector3[] vertices, normals;
 	Vector4[] tangents;
 	
@@ -54,23 +60,45 @@ public class ProceduralMesh : MonoBehaviour {
 		GenerateMesh();
 		enabled = false;
 
-		vertices = mesh.vertices;
-		normals = mesh.normals;
-		tangents = mesh.tangents;
+		vertices = null;
+		normals = null;
+		tangents = null;
 	}
 
 	void OnDrawGizmos () {
-		if (mesh == null) {
+		if (gizmos == GizmoMode.Nothing || mesh == null) {
 			return;
 		}
+
+		bool drawVertices = (gizmos & GizmoMode.Vertices) != 0;
+		bool drawNormals = (gizmos & GizmoMode.Normals) != 0;
+		bool drawTangents = (gizmos & GizmoMode.Tangents) != 0;
+
+		if (vertices == null) {
+			vertices = mesh.vertices;
+		}
+		if (drawNormals && normals == null) {
+			normals = mesh.normals;
+		}
+		if (drawTangents && tangents == null) {
+			tangents = mesh.tangents;
+		}
+
+		Transform t = transform;
 		for (int i = 0; i < vertices.Length; i++) {
-			Vector3 position = vertices[i];
-			Gizmos.color = Color.cyan;
-			Gizmos.DrawSphere(position, 0.02f);
-			Gizmos.color = Color.green;
-			Gizmos.DrawRay(position, normals[i] * 0.2f);
-			Gizmos.color = Color.red;
-			Gizmos.DrawRay(position, tangents[i] * 0.2f);
+			Vector3 position = t.TransformPoint(vertices[i]);
+			if (drawVertices) {
+				Gizmos.color = Color.cyan;
+				Gizmos.DrawSphere(position, 0.02f);
+			}
+			if (drawNormals) {
+				Gizmos.color = Color.green;
+				Gizmos.DrawRay(position, t.TransformDirection(normals[i]) * 0.2f);
+			}
+			if (drawTangents) {
+				Gizmos.color = Color.red;
+				Gizmos.DrawRay(position, t.TransformDirection(tangents[i]) * 0.2f);
+			}
 		}
 	}
 
