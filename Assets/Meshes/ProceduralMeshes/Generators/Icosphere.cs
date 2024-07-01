b345ff18d70210876eef6beb9f0b30e660723176
Daniel Pascual López <uo269728@uniovi.es>
2024-07-01 19:01:18 +0200

    erro icosaedro

--- Assets/Meshes/ProceduralMeshes/Generators/Icosphere.cs
+++ Assets/Meshes/ProceduralMeshes/Generators/Icosphere.cs
@@ -8,9 +8,10 @@ namespace ProceduralMeshes.Generators
 
 	public struct Icosphere  : IMeshGenerator
 	{
-		public int VertexCount => 5 * Resolution * Resolution + 2;
+		int ResolutionV => 2 * Resolution;
+		public int VertexCount => 5 * ResolutionV * Resolution + 2;
 
-		public int IndexCount => 6 * 5 * Resolution * Resolution;
+		public int IndexCount => 6 * 5 * ResolutionV * Resolution;
 
 		public int JobLength => 5 * Resolution;
 
@@ -24,39 +25,26 @@ namespace ProceduralMeshes.Generators
 			public bool TouchesMinimumPole => (id & 1) == 0;
 		}
 
-		struct Rhombus
+		struct Strip 
 		{
 			public int id;
-			public float3 leftCorner, rightCorner;
+			public float3 lowLeftCorner, lowRightCorner, highLeftCorner, highRightCorner;
 		}
 
-		static Rhombus GetRhombus(int id) => id switch
-		{
-			0 => new Rhombus {
-				id = id,
-				leftCorner = GetCorner(0),
-				rightCorner = GetCorner(1)
-			},
-			1 => new Rhombus {
-				id = id,
-				leftCorner = GetCorner(1),
-				rightCorner = GetCorner(2)
-			},
-			2 => new Rhombus {
-				id = id,
-				leftCorner = GetCorner(2),
-				rightCorner = GetCorner(3)
-			},
-			3 => new Rhombus {
-				id = id,
-				leftCorner = GetCorner(3),
-				rightCorner = GetCorner(4)
-			},
-			_ => new Rhombus {
-				id = id,
-				leftCorner = GetCorner(4),
-				rightCorner = GetCorner(0)
-			}
+		static Strip GetStrip (int id) => id switch {
+			0 => CreateStrip(0),
+			1 => CreateStrip(1),
+			2 => CreateStrip(2),
+			3 => CreateStrip(3),
+			_ => CreateStrip(4)
+		};
+
+		static Strip CreateStrip (int id) => new Strip {
+			id = id,
+			lowLeftCorner = GetCorner(2 * id, -1),
+			lowRightCorner = GetCorner(id == 4 ? 0 : 2 * id + 2, -1),
+			highLeftCorner = GetCorner(id == 0 ? 9 : 2 * id - 1, 1),
+			highRightCorner = GetCorner(2 * id + 1, 1)
 		};
 
 		static float3 CubeToSphere(float3 p) => p * sqrt(
@@ -66,42 +54,52 @@ namespace ProceduralMeshes.Generators
 		public Bounds Bounds => new Bounds(Vector3.zero, new Vector3(2f, 2f, 2f));
 		
 
-		static float3 GetCorner (int id) => float3(
-			sin(0.4f * PI * id),
-			0f,
-			-cos(0.4f * PI * id)
+		static float3 GetCorner (int id, int ySign) => float3(
+			0.4f * sqrt(5f) * sin(0.2f * PI * id),
+			ySign * 0.2f * sqrt(5f),
+			-0.4f * sqrt(5f) * cos(0.2f * PI * id)
 		);
 	
 		public void Execute<S>(int i, S streams) where S : struct, IMeshStreams
 		{
-			int u = i / 4;
-			Rhombus rhombus = GetRhombus(i - 4 * u);
-			int vi = Resolution * (Resolution * rhombus.id + u ) + 2;
-			int ti = 2 * Resolution * (Resolution * rhombus.id + u);
+			int u = i / 5;
+			Strip strip = GetStrip(i - 5 * u);
+			int vi = ResolutionV * (Resolution * strip.id + u) + 2;
+			int ti = 2 * ResolutionV * (Resolution * strip.id + u);
 			bool firstColumn = u == 0;
 
 			int4 quad = int4(
 				vi,
-				firstColumn ? 0 : vi - Resolution,
+				firstColumn ? 0 : vi - ResolutionV,
 				firstColumn ?
-					rhombus.id == 0 ?
-						3 * Resolution * Resolution + 2 :
-						vi - Resolution * (Resolution + u) :
-					vi - Resolution + 1,
+					strip.id == 0 ?
+						4 * ResolutionV * Resolution + 2 :
+						vi - ResolutionV * (Resolution + u) :
+					vi - ResolutionV + 1,
 				vi + 1
 			);
 
 			u += 1;
 
-			float3 columnBottomDir = rhombus.rightCorner - down();
+			float3 columnBottomDir = strip.lowRightCorner - down();
 			float3 columnBottomStart = down() + columnBottomDir * u / Resolution;
 			float3 columnBottomEnd =
-				rhombus.leftCorner + columnBottomDir * u / Resolution;
+				strip.lowLeftCorner + columnBottomDir * u / Resolution;
 
-			float3 columnTopDir = up() - rhombus.leftCorner;
+			float3 columnLowDir = strip.highRightCorner - strip.lowLeftCorner;
+			float3 columnLowStart =
+				strip.lowRightCorner + columnLowDir * ((float)u / Resolution - 1f);
+			float3 columnLowEnd = strip.lowLeftCorner + columnLowDir * u / Resolution;
+
+			float3 columnHighDir = strip.highRightCorner - strip.lowLeftCorner;
+			float3 columnHighStart = strip.lowLeftCorner + columnHighDir * u / Resolution;
+			float3 columnHighEnd = strip.highLeftCorner + columnHighDir * u / Resolution;
+
+
+			float3 columnTopDir = up() - strip.highLeftCorner;
 			float3 columnTopStart =
-				rhombus.rightCorner + columnTopDir * ((float)u / Resolution - 1f);
-			float3 columnTopEnd = rhombus.leftCorner + columnTopDir * u / Resolution;
+				strip.highRightCorner + columnTopDir * ((float)u / Resolution - 1f);
+			float3 columnTopEnd = strip.highLeftCorner + columnTopDir * u / Resolution;
 
 			var vertex = new Vertex();
 			if (i == 0) {
@@ -114,17 +112,23 @@ namespace ProceduralMeshes.Generators
 			streams.SetVertex(vi, vertex);
 			vi += 1;
 
-			for (int v = 1; v < Resolution; v++, vi++, ti += 2)
+			for (int v = 1; v < ResolutionV; v++, vi++, ti += 2) 
 			{
 				if (v <= Resolution - u)
 				{
 					vertex.position =
 						lerp(columnBottomStart, columnBottomEnd, (float)v / Resolution);
 				}
+				else if (v < Resolution) {
+					vertex.position = 0f;
+				}
+				else if (v <= ResolutionV - u) {
+					vertex.position =lerp(columnHighStart, columnHighEnd, (float)v / Resolution - 1f);
+				}
 				else
 				{
 					vertex.position =
-						lerp(columnTopStart, columnTopEnd, (float)v / Resolution);
+						lerp(columnTopStart, columnTopEnd, (float)v / Resolution - 1f);
 				}
 				
 				
@@ -133,13 +137,15 @@ namespace ProceduralMeshes.Generators
 				streams.SetTriangle(ti + 1, quad.xzw);
 
 				quad.y = quad.z;
-				quad += int4(1, 0, firstColumn  ? Resolution : 1, 1);
+				quad +=
+					int4(1, 0, firstColumn && v <= Resolution - u ? ResolutionV : 1, 1);
 			}
 
 			if (!firstColumn) {
-				quad.z = Resolution * Resolution * (rhombus.id == 0 ? 4 : rhombus.id) -
+				quad.z = ResolutionV * Resolution * (strip.id == 0 ? 5 : strip.id) -
 					Resolution + u + 1;
 			}
+
 			quad.w = u < Resolution ? quad.z + 1 : 1;
 
 			streams.SetTriangle(ti + 0, quad.xyz);
